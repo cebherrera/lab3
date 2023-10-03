b571ff5d8ec41d08f1f5094882cce8e8121a591b
Sebastian <64702743+cebherrera@users.noreply.github.com>
2023-10-03 10:21:52 -0600

    se agrega gitignore, y switch, asi como pruebas de electronica adicional

---
+++ voltimetro/voltimetro.ino
@@ -0,0 +1,14 @@
+
+
+void setup() {
+  // se inicializa pin digital que servira para seleccionar el modo de medicion
+
+
+}
+
+void loop() {
+
+
+  // put your main code here, to run repeatedly:
+
+}
